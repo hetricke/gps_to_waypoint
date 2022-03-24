b5754c0b305bb6ad84ce3ad6aed57c5e6c961349
Emma Hetrick <hetricke@umich.edu>
2022-03-23 20:29:07 -0400

    added structs to convert.hpp

---
+++ include/convert.hpp
@@ -0,0 +1,20 @@
+struct waypoint{
+    double x;
+    double y;
+    double z;
+
+    double roll;
+    double pitch;
+    double yaw;
+
+    double tolerance;
+};
+
+struct gps_coordinates{
+
+    double latitude;
+    double longitude;
+    double orientation;
+
+};
+
---
+++ include/readFile.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "convert.hpp"
+#include "vector"
+
+
+class readFile{
+
+readFile();
+
+std::vector<gps_coordinates> read();
+
+
+};
