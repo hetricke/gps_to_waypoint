aa1da0046e017986da90a4affa380a5b1e54d7bd
EJ05 <eejoylim@gmail.com>
2022-04-04 18:53:43 -0400

    deleted convert.cpp & updated convert.hpp

--- include/convert.hpp
+++ include/convert.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <numbers>
 
 struct Waypoint {
     double x;
@@ -20,31 +21,66 @@ struct GPSCoordinates {
 
 };
 
-class Convert {
-public:
-    static Convert convert_gps_to_waypoint(const std::vector<GPSCoordinates>&);
 
-    std::vector<Waypoint> get_waypoints();
+namespace converter {
+    double get_x(double gps_x, double origin_x) {
+        return gps_x - origin_x;
+    }
+    double get_y(double gps_y, double origin_y) {
+        return gps_y - origin_y;
+    }
 
-    double get_x(double);
+    double get_yaw(double angle) {
+        return angle * std::numbers::pi / 180;
+    }
 
-    double get_y(double);
+    std::vector<Waypoint> convert_gps_to_waypoint(std::vector<GPSCoordinates>& gps_coordinates_list) {
+        Waypoint temp_waypoint;
+        std::vector<Waypoint> waypoints;
 
-    double get_yaw(double);
+        double origin_x{}, x{};
+        double origin_y{}, y{};
+        double yaw{};
 
-private:
+        // constant??
+        double z = 0;
+        double roll = 0;
+        double pitch = 0;
+        double tolerance = 0.3;
 
-    Convert() = default;
+        if (!gps_coordinates_list.empty()) {
+            for (auto temp_gps: gps_coordinates_list) {
+                if (waypoints.empty()) {
+                    // assign the first GPS coordinate in the list as origin, then convert to waypoint
+                    origin_x = temp_gps.latitude;
+                    origin_y = temp_gps.longitude;
+                    yaw = get_yaw(temp_gps.orientation);
+                    temp_waypoint = {0, 0, z, roll, pitch, yaw, tolerance};
+                    waypoints.push_back(temp_waypoint);
+                }
+                else {
+                    // convert the remaining GPS coordinates in the list to waypoint
+                    x = get_x(temp_gps.latitude, origin_x);
+                    y = get_y(temp_gps.longitude, origin_y);
+                    yaw = get_yaw(temp_gps.orientation);
 
-    double origin_x_{}, x_{};
-    double origin_y_{}, y_{};
-    double yaw_{};
+                    temp_waypoint = {x, y, z, roll, pitch, yaw, tolerance};
+                    waypoints.push_back(temp_waypoint);
+                }
+            }
+        }
 
-    // constant??
-    double z_ = 0;
-    double roll_ = 0;
-    double pitch_ = 0;
-    double tolerance_ = 0.3;
+        /*
+        CONVERT CARTESIAN TO SPHERICAL COORDINATE
+        Cartesian (x, y, z)
+        Spherical (r, theta, phi)
 
-    std::vector<Waypoint> waypoints;
-};
+        r = sqrt((x * x) + (y * y) + (z * z))
+        theta = arctan(y / x)
+        phi = arctan(sqrt((x * x) + (y * y)) / z)
+        */ 
+
+
+        return waypoints;
+    }
+} // namepsace converter
