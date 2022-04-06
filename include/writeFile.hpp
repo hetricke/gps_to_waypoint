5fd7d3f533a371f167a46d39fb23fe16ac0e1ae2
salmart <salatiel@umich.edu>
2022-04-06 19:39:07 -0400

    Updated writeFile.hpp

---
+++ include/writeFile.hpp
@@ -0,0 +1,32 @@
+#include "convert.hpp"
+#include "readFile.hpp"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace WriteFile {
+    void write(const std::string& file_path){
+        std::ofstream file("waypoints.csv");
+
+        if (file.is_open()){
+    
+            std::vector<Waypoint> pile;
+            pile = convert_gps_to_waypoint(ReadFile::read(filepath)));
+
+            file<< "X,Y,Z,Roll,Pitch,Yaw,Tolerance\n";
+
+            for (int i =0; i<pile.size();++i){
+
+                file<< pile.at(i).x<<","<<pile.at(i).y<<","
+                <<pile.at(i).z<<","<< pile.at(i).roll<<","<<pile.at(i).pitch
+                <<","<<pile.at(i).yaw<<","<<pile.at(i).tolerance<<std::endl;
+            }
+        } 
+        else {
+
+            std::cout<<"File could not open"<<std::endl;
+            return;
+        } 
+    }
+} // namespace writefile
