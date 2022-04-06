53b83e580514b3cccb9c8af9022e78a6282d8713
Emma Hetrick <hetricke@umich.edu>
2022-04-06 18:59:57 -0400

    fixed cmake

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,6 @@
 
 int main(int argc, char *argv[]) {
 
-    std::vector<GPSCoordinates> result = ReadFile::read("/home/hetricke/Documents/ISC/Testing/gps_test_read.txt");
-
-
-    for (int i = 0; i < result.size(); i++) {
-        printf("%lf, %lf, %lf", result[i].longitude, result[i].latitude, result[i].orientation);
-    }
-
-    auto converted = converter::convert_gps_to_waypoint(result);
 
     return 0;
 }
