b026fbecae5f887ab490d3fd56df3410c2d974a2
Emma Hetrick <hetricke@umich.edu>
2022-04-06 20:20:15 -0400

    Works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "../include/readFile.hpp"
 #include "convert.hpp"
+#include <string>
 //#include "fmt/core.h"
 
 
 int main(int argc, char *argv[]) {
 
-    std::vector<GPSCoordinates> result = ReadFile::read("/home/hetricke/Documents/ISC/Testing/gps_test_read.txt");
+    std::string fp = "/home/hetricke/Documents/ISC/Testing/gps_test_read";
+    std::vector<GPSCoordinates> result = ReadFile::read(fp);
 
 
     for (int i = 0; i < result.size(); i++) {
